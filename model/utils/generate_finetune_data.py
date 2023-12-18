a830077714c639772c2d1b66ce5b5e04451f96ae
mumuwithw <57976506+mumuwithw@users.noreply.github.com>
2023-12-18 16:27:52 +0800

    Update generate_finetune_data.py

--- model/utils/generate_finetune_data.py
+++ model/utils/generate_finetune_data.py
@@ -42,50 +42,5 @@ def generate_template():
             f.write(s + '\n')
     all_df.to_csv(output_csv_file_name)
 
-def generate_pair():
-    # 生成序列对 -> NSP
-    df = pd.read_csv(structed_file_name)
-    train_size=100000
-    val_size = 10000
-    fake_rate = 0.5
-    train_list = get_NSP_item(df, train_size, fake_rate)
-    # val_list = get_NSP_item(df, val_size, fake_rate)
-
-    with open(train_NSP_out_name, 'w') as f:
-        for i in train_list:
-            f.write(json.dumps(i))
-            f.write('\n')
-    
-    # with open(val_NSP_out_name, 'w') as f:
-    #     for i in val_list:
-    #         f.write(json.dumps(i))
-    #         f.write('\n')
-        
-
-def get_NSP_item(df, size, fake_rate):
-    data_list = []
-    for i in range(size):
-        idx = random.randint(0, df.shape[0]-1)
-        # text_a 确保是一条正常的日志
-        while(df.iloc[idx].Label!="-"):
-            idx = random.randint(0, df.shape[0])
-        text_a = camel_segment(df.iloc[idx].EventTemplate)
-
-        if random.random()>fake_rate:
-            # 直接取text_a的下一条日志，label为1（除非下一条日志是异常日志）
-            text_b = camel_segment(df.iloc[idx+1].EventTemplate)
-            label = 1 if df.iloc[idx+1].Label=="-" else 0
-        else:
-            # 由于数量足够大，基本就算不是相邻的了
-            idx_b = random.randint(0, df.shape[0])
-            text_b = camel_segment(df.iloc[idx_b].EventTemplate)
-            label = 0
-        data_list.append({
-            "text_a": text_a,
-            "text_b": text_b,
-            "label": label,
-        })
-    return data_list
 
 generate_template()
-# generate_pair()
