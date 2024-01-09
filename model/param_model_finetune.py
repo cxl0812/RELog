71ee12fcac75ae62aa3db813dd07627aa4d2a6de
mumuwithw <57976506+mumuwithw@users.noreply.github.com>
2024-01-09 16:49:30 +0800

    Update param_model_finetune.py

--- model/param_model_finetune.py
+++ model/param_model_finetune.py
@@ -25,15 +25,7 @@ def get_model():
 
     print(len(model.words))
 
-def test_model():
-    model = fasttext.load_model(model_path)
-    print(model[''])
-    # for i in range(1000000):  # 2m1.519s
-        # a = model['/user/root/rand/_logs/history/ip-10-250-19-102.ec2.internal_1226291400491_job_200811092030_0001_conf.xml.']
-        # a = model['/user/root/rand/_logs/history/ip-10-250-19-102.ec2.internal_1226291400491_job_200811092030_0001_conf.xml.']
-
 
 if __name__ == "__main__":
     get_model()
-    # test_model()
     
